04d29e50659e36f23d132ce8cc10c66ce7f5ace6
Rasim2024 <r6600666@gmail.com>
2024-06-20 15:33:24 +0300

    На проверку

--- vite-project/src/components/Header/Header.jsx
+++ vite-project/src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-
+import PopUser from "../Popups/PopUser/PopUser "
 
 const Header = () => {
     return (
@@ -22,21 +22,7 @@ const Header = () => {
                 <a href="#user-set-target" className="header__user _hover02">
                   Ivan Ivanov
                 </a>
-                <div
-                  className="header__pop-user-set pop-user-set"
-                  id="user-set-target"
-                >
-                  {/* <a href="">x</a> */}
-                  <p className="pop-user-set__name">Ivan Ivanov</p>
-                  <p className="pop-user-set__mail">ivan.ivanov@gmail.com</p>
-                  <div className="pop-user-set__theme">
-                    <p>Темная тема</p>
-                    <input type="checkbox" className="checkbox" name="checkbox" />
-                  </div>
-                  <button type="button" className="_hover03">
-                    <a href="#popExit">Выйти</a>
-                  </button>
-                </div>
+                <PopUser/>
               </nav>
             </div>
           </div>
---
+++ vite-project/src/components/Main/Main.jsx
@@ -0,0 +1,20 @@
+import Card from "../Card/Card"
+import Column from "../Column/Column"
+
+
+
+function Main () {
+    return (
+        <main className="main">
+        <div className="container">
+          <div className="main__block">
+            <div className="main__content">
+             <Column/>
+            </div>
+          </div>
+        </div>
+      </main>
+    )
+}
+
+export default Main
