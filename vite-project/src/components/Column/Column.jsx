fa1603087da8044dd7abd03b0eb1de8e6eb08ead
Rasim2024 <r6600666@gmail.com>
2024-06-26 17:50:21 +0300

    Задание-1

--- vite-project/src/components/Column/Column.jsx
+++ vite-project/src/components/Column/Column.jsx
@@ -1,25 +1,17 @@
 import React from "react";
 import Card from "../Card/Card";
 
-
-function Column (props) {
- 
-    return (
-
-             <div className="main__column column">
-                    <div className="column__title">
-                      <p>{props.status}</p>
-                    </div>
-                    <div className="cards">
-                      <Card/>
-                                    
-                    </div>
-                  </div>
-                  
-      
-       )
-  }
-   
-
+function Column(props) {
+  return (
+    <div className="main__column column">
+      <div className="column__title">
+        <p>{props.status}</p>
+      </div>
+      <div className="cards">
+        <Card />
+      </div>
+    </div>
+  );
+}
 
 export default Column;
