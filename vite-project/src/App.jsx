8292fb2e8cfb0fbff8e3cae7bf2b8adb87f0eaf2
Rasim2024 <r6600666@gmail.com>
2024-06-27 19:27:41 +0300

    Задание выполнено

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,8 +4,31 @@ import PopBrowse from './components/Popups/PopBrowse/PopBrowse.jsx';
 import PopNewCard from './components/Popups/PopNewCard/PopNewCard.jsx';
 import Main from './components/Main/Main.jsx';
 import PopExit from './components/Popups/PopExit.jsx';
+import { useEffect, useState } from "react";
+import { cardList } from './Data.js';
 
 function App() {
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [cards, setCards] = useState(cardList)
+ 
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000)
+  }, []);
+
+  function addCard () {
+    const newTask = {
+      id: cards.length + 1,
+      topic: "Web Design",
+      title: "Название задачи",
+      date: "27.06.24",
+      status: "Без статуса",
+    }
+    setCards([...cards, newTask])
+      }
 
   return (
     <>
@@ -16,7 +39,7 @@ function App() {
         <PopBrowse/>
         {/* pop-up end*/}
         <Header/>    {/* Выгружается с хедер*/}
-       <Main/>
+        {isLoading ? <h1 className='load'>Данные загружаются...</h1> : <Main cardList={cards} />}
       </div>
 
     </>
